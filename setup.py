7472de43d6ccb173ebf51a0b04ab131330a9a8d2
jbs.public.function@gmail.com <jbs.public.function@gmail.com>
2020-03-27 18:42:03 -0500

    fixing .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='vis',
+    version='0.1dev',
+    # packages = ['vis'],
+    # package_dir={'vis': '\\vis\\vis'},
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+)
+
