f20355b641b86864cc6a3cd61439d578ef326a4b
jbs.public.function@gmail.com <jbs.public.function@gmail.com>
2020-03-28 07:29:21 -0500

    initial_color worked out some kinks on matplotlib and plotly plotting as far as setting up colorbars. I have some thoughts I need to push up soon but I've spent too much time on this already

--- vis/code/color/colorlover_utils.py
+++ vis/code/color/colorlover_utils.py
@@ -20,6 +20,7 @@ To convert back from hex to rgb, you reverse the process.
 
 def hex_string_to_rgb(h):
     """
+    I got this from stackoverflow
     :param h: str '#FFFFFF' to convert from hex to rgb
     :return: rgb tuple
     """ 
@@ -35,6 +36,40 @@ def rgb_to_hex_string(r):
     return f'#{conversion}'
 
 
+def rgb_string_to_tuple(r):
+    """
+    If I need to convert a plotly rgb string `'rgb(0,0,0)'`
+    I have to strip manipulate the string
+    :param r: string repr of rgb value, most likely from plotly
+    :return: tuple(int, int, int)
+    """
+    rgb = r.split('(')[-1].split(')')[0].split(',')
+    if len(rgb) > 4 or len(rgb) < 3:
+        raise ValueError
+    return tuple((int(i) for i in rgb))
+
+
+def minmax_scale(v):
+    """
+    scale v by 255, 0
+    :param v: int
+    :return: float <= 1
+    """
+    return (v - 0) / 255
+
+
+def minmax_scale_rgb(r):
+    """
+    matplotlib wants rgb as scaled ratios
+    (255, 0, 128) -> (1, 0, .5)
+    will also accept rgba but no scaling performed since it should be a 0..1 value on input
+    """
+    rgb = []
+    for _rgb in r:
+        rgb.append([minmax_scale(i) for i in _rgb[:3] + _rgb[3:]])
+    return rgb
+
+
 def rgb_to_html_display(name, rgb):
     """
     colorlover cl.to_html returns a div based on the input color
@@ -45,4 +80,3 @@ def rgb_to_html_display(name, rgb):
     """
     rgb_as_html = cl.to_html([rgb])
     return '<div title="{}" {}'.format(name, rgb_as_html.split('<div ')[-1])
-
